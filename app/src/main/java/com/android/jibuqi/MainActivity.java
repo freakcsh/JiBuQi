90b710fb9d48004ac98532d557b966795033ad8e
freakcsh <740997937@qq.com>
2017-07-03 12:52:49 +0800

    第一次修改

--- app/src/main/java/com/android/jibuqi/MainActivity.java
+++ app/src/main/java/com/android/jibuqi/MainActivity.java
@@ -4,7 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+/*
+我在这里添加注释
+ */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
